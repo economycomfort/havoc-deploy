894cd6fdc81cf5b060bc0563812aa538fba0bf1d
Tom D'Aquino <tom@havocops.io>
2021-05-29 16:45:21 -0700

    Removed quoted references in depends_on for api gateway deployment

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -7,7 +7,7 @@ resource "aws_api_gateway_rest_api" "rest_api" {
 
 resource "aws_api_gateway_deployment" "rest_api" {
   rest_api_id = aws_api_gateway_rest_api.rest_api.id
-  depends_on = ["aws_api_gateway_method.manage_post", "aws_api_gateway_method.remote_task_post", "aws_api_gateway_method.task_control_post"]
+  depends_on = [aws_api_gateway_method.manage_post, aws_api_gateway_method.remote_task_post, aws_api_gateway_method.task_control_post]
 
   triggers = {
     redeployment = sha1(jsonencode(aws_api_gateway_rest_api.rest_api.body))
