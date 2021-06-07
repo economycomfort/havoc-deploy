2fadaed536642ec39a996024e473f25d005d871c
Tom D'Aquino <tom@havocops.io>
2021-06-06 23:59:39 -0700

    Added timeout for task_control lambda

--- terraform/lambdas.tf
+++ terraform/lambdas.tf
@@ -111,6 +111,7 @@ resource "aws_lambda_function" "task_control" {
 
   handler = "lambda_function.lambda_handler"
   runtime = "python3.8"
+  timeout = 30
 
   role = aws_iam_role.lambda_role.arn
 
