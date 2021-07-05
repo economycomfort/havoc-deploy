e54a5adbdff398cc30df10a714e968d4971641ce
Tom D'Aquino <tom@havocops.io>
2021-07-04 18:28:55 -0700

    Modified filter_pattern to use instruct_command_output

--- terraform/cw_logs.tf
+++ terraform/cw_logs.tf
@@ -5,6 +5,6 @@ resource "aws_cloudwatch_log_group" "ecs_task_logs" {
 resource "aws_cloudwatch_log_subscription_filter" "task_result_lambdafunction_logfilter" {
   name            = "task_result_lambdafunction_logfilter"
   log_group_name  = aws_cloudwatch_log_group.ecs_task_logs.name
-  filter_pattern  = "task_response user_id task_name"
+  filter_pattern  = "instruct_command_output user_id task_name"
   destination_arn = aws_lambda_function.task_result.arn
 }
