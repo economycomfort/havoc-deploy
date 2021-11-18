7892fd1046953e35c3726de94edf3616c4f07ac8
Tom D'Aquino <tom@havocops.io>
2021-11-18 01:16:34 -0800

    Extended task_result function timeout

--- aws/terraform/lambdas.tf
+++ aws/terraform/lambdas.tf
@@ -145,7 +145,7 @@ resource "aws_lambda_function" "task_result" {
 
   handler = "lambda_function.lambda_handler"
   runtime = "python3.8"
-  timeout = 30
+  timeout = 60
 
   role = aws_iam_role.lambda_role.arn
 
